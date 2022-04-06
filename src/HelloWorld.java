a915bd2f660653dc564fbd82fb49ce389849fd7a
Jordan Foote <82241562+jordanfoote@users.noreply.github.com>
2022-04-06 15:19:42 -0500

    Updating

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,20 @@ public class HelloWorld {
 
     public static void main (String[] args){
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 1;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Jordan";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+
     }
 }
