85d9930c5af66f617db7237765d64264aa28dcd4
jordanfoote <jordan.foote@outlook.com>
2022-04-07 12:02:58 -0500

    finished fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,10 +2,34 @@ public class ControlFlowExercises {
 
     public static void main (String[] args) {
 
-        int i = 5;
-         while (i <= 15) {
-             System.out.println(i);
-             i++;
-         }
+        // int i = 5;
+        // while (i <= 15) {
+        //      System.out.println(i);
+        //      i++;
+        // }
+
+        // double x = 2;
+        // do {
+        //     System.out.println(x);
+        //     x *= x;
+        // } while (x <= 1000000);
+
+        // for (double y = 2; y <= 1000000; y *=y ) {
+        //     System.out.println(y);
+        // }
+
+        // for (int q = 1; q <= 100; q++) {
+        //     if (q % 3 == 0 & q % 5 == 0) {
+        //         System.out.println("FizzBuzz");
+        //     } else if (q % 5 == 0) {
+        //         System.out.println("Buzz");
+        //     } else if (q % 3 == 0) {
+        //         System.out.println("Fizz");
+        //     } else {
+        //         System.out.println(q);
+        //     }
+        // }
+
+        
     }
 }
