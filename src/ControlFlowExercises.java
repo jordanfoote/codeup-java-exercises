ecec7052d9eabadccc81ac7e4db4daab23bf4e47
Jordan Foote <82241562+jordanfoote@users.noreply.github.com>
2022-04-07 14:14:48 -0500

    completed control statements and loops exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 
     public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         // int i = 5;
         // while (i <= 15) {
@@ -30,6 +34,29 @@ public class ControlFlowExercises {
         //     }
         // }
 
-        
+        // System.out.println("Please enter a whole number: ");
+        // int userInput = scanner.nextInt();
+        // System.out.println("number | squared | cubed");
+        // System.out.println("------ | ------- | -----");
+        // for (int i = 1; i <= userInput; i++) {
+        //     int square = i * i;
+        //     int cube = i * i * i;
+        //     System.out.printf("%s      | %s      |%s%n", i, square, cube);
+        // }
+
+        // System.out.println("Please enter a grade: ");
+        // int numGrade = scanner.nextInt();
+        // if (numGrade < 60) {
+        //     System.out.println("F");
+        // } else if (numGrade < 67) {
+        //     System.out.println("D");
+        // } else if (numGrade < 80) {
+        //     System.out.println("C");
+        // } else if (numGrade < 88) {
+        //     System.out.println("B");
+        // } else {
+        //     System.out.println("A");
+        // }
+
     }
 }
