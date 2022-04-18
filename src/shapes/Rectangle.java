e88a8bf98d7c664e1257143fdf922698140ce0cd
Jordan Foote <82241562+jordanfoote@users.noreply.github.com>
2022-04-18 14:31:56 -0500

    updating

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -2,19 +2,22 @@ package shapes;
 
 public class Rectangle {
 
-    protected double length;
-    protected double width;
+    // protected double length;
+    // protected double width;
+//
+    // public Rectangle(double length, double width) {
+    //     this.length = length;
+    //     this.width = width;
+    // }
+//
+    // public double getArea() {
+    //     return this.length * this.width;
+    // }
+//
+    // public double getPerimeter() {
+        // return (2 * length) + (2 * width);
+    // }
 
-    public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
-    }
 
-    public double getArea() {
-        return this.length * this.width;
-    }
 
-    public double getPerimeter() {
-        return (2 * length) + (2 * width);
-    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,7 +2,9 @@ package shapes;
 
 public class Square extends Rectangle {
 
-    public Square(double side) {
-        super(side, side);
-    }
+    // public Square(double side) {
+    //     super(side, side);
+    // }
+
+
 }
